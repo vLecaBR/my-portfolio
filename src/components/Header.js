d4b4d24a1da53193b5d9ce2f4a785976564fcf3b
vLecaBR <vitartasleca@gmail.com>
2024-08-06 16:57:13 -0300

    applied responsiveness in header and hero

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,7 @@ const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 20px 50px;
+  padding: 20px 20px;
   background-color: #0a192f;
   color: #64ffda;
   position: fixed;
@@ -54,6 +54,10 @@ const Logo = styled.div`
   border: 1px solid #64ffda;
   padding: 10px;
   border-radius: 5px;
+
+  @media (max-width: 480px) {
+    margin-left: 0px;
+  }
 `;
 
 const Menu = styled.div`
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,10 +7,10 @@ const Hero = () => (
     <Name>Victor Leça.</Name>
     <Subtitle>I build things for the web.</Subtitle>
     <Description>
-    A young computer science student focused on web development, with professional experience primarily in technical support. <br></br>
-    Currently seeking experience in the web development field. <br></br>
-    Quick to learn and adept at working with the latest web technologies, such as React.JS, HTML5, CSS3, SASS, Figma, Svelte, Python, and more. <br></br>
-    Always eager to learn more.
+      A young computer science student focused on web development, with professional experience primarily in technical support. <br></br>
+      Currently seeking experience in the web development field. <br></br>
+      Quick to learn and adept at working with the latest web technologies, such as React.JS, HTML5, CSS3, SASS, Figma, Svelte, Python, and more. <br></br>
+      Always eager to learn more.
     </Description>
     <Button href="https://www.linkedin.com/in/victor-leca-vlkbr/">Check out my Linkedin</Button>
   </Section>
@@ -26,26 +26,58 @@ const Section = styled.section`
   background-color: #0a192f;
   color: #ccd6f6;
   z-index: 1;
+
+  @media (max-width: 768px) {
+    padding: 0 50px;
+  }
+
+  @media (max-width: 480px) {
+    padding: 0 20px;
+  }
 `;
 
 const Intro = styled.h1`
   color: #64ffda;
+
+  @media (max-width: 480px) {
+    font-size: 1.5em;
+  }
 `;
 
 const Name = styled.h2`
   font-size: 3.5em;
   margin: 0;
+
+  @media (max-width: 768px) {
+    font-size: 3em;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 2.5em;
+  }
 `;
 
 const Subtitle = styled.h3`
   font-size: 2.5em;
   margin: 10px 0;
+
+  @media (max-width: 768px) {
+    font-size: 2em;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 1.75em;
+  }
 `;
 
 const Description = styled.p`
   max-width: 540px;
   line-height: 1.5;
   margin: 20px 0;
+
+  @media (max-width: 480px) {
+    font-size: 0.9em;
+  }
 `;
 
 const Button = styled.a`
@@ -55,6 +87,11 @@ const Button = styled.a`
   border-radius: 4px;
   color: #64ffda;
   text-decoration: none;
+
+  @media (max-width: 480px) {
+    padding: 10px 15px;
+    font-size: 0.9em;
+  }
 `;
 
 export default Hero;
