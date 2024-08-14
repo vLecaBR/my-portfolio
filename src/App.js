8705e1f8435a3ec39c1e3f577801ae968a2d89cf
vlecabr <vitartasleca@gmail.com>
2024-08-14 08:46:50 -0300

    teste remove mouse glow

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Sidebar from './components/Sidebar';
-import MouseGlow from './components/MouseGlow';
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const App = () => (
   <>
-    <MouseGlow />
     <Header />
     <Hero id="home" />
     <About />
