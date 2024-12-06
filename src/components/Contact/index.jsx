af7fac2cdcb9607d649a054c047608ad88bbd629
vlecabr <vitartasleca@gmail.com>
2024-12-06 16:16:22 -0300

    contacts updated to open a _blank tab

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -12,10 +12,10 @@ const Contact = () => (
     <Title>Contact</Title>
     <Text>
       <h4>
-        Linkedin <Email href="https://www.linkedin.com/in/victor-leca-vlkbr/">Victor Leça</Email>. <br></br>
-        GitHub <Email href="https://github.com/vLecaBR">vLecaBR</Email>. <br></br>
-        Phone <Email href="tel:+5516988647864">+5516988647864 (WhatsApp)</Email>. <br></br>
-        Email <Email href="mailto:vitartasleca@gmail.com">vitartasleca@gmail.com</Email>.
+        Linkedin <Email href="https://www.linkedin.com/in/victor-leca-vlkbr/" target="_blank" rel="noopener noreferrer" >Victor Leça</Email>. <br></br>
+        GitHub <Email href="https://github.com/vLecaBR" target="_blank" rel="noopener noreferrer" >vLecaBR</Email>. <br></br>
+        Phone <Email href="tel:+5516988647864" target="_blank" rel="noopener noreferrer" >+5516988647864 (WhatsApp)</Email>. <br></br>
+        Email <Email href="mailto:vitartasleca@gmail.com" target="_blank" rel="noopener noreferrer" >vitartasleca@gmail.com</Email>.
       </h4>
     </Text>
   </Section>
