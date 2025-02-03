b099723eebada6903a13edddbad3be54e4e9c359
vlecabr <vitartasleca@gmail.com>
2025-02-03 11:01:45 -0300

    atualizado about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -10,13 +10,17 @@ const About = () => (
   <Section id="about">
     <Title>About Me</Title>
     <Text>
-    I am a Computer Science student with 3 years of experience in multinational companies, focusing on technical support and infrastructure
-    since 2021. Currently, I work directly with programming in the automation sector, where I am responsible for creating websites using ReactJS, 
-    Tailwind CSS, and other technologies. I also develop applications, automation flows, lists, and libraries through the Power Platform, utilizing
-     technologies such as Power Apps, Power Automate, and Office 365 tools, as well as managing SharePoint flows and pages.
-    I have a strong passion for technology and development, especially in the area of web development. I possess skills in JavaScript, HTML, CSS, ReactJS, 
-    Svelte, Figma and Python. Additionally, I am actively learning and expanding my knowledge in popular frameworks and libraries like Angular and ReactJS.
-    I am known for being quick, a fast and consistent learner, always motivated to overcome obstacles and embrace new challenges.
+    I am a Computer Science student with 4 years of experience in multinational companies, 
+    focusing on technical support and infrastructure since 2021. Currently, I work directly 
+    with programming in the automation sector, where I am responsible for creating websites 
+    using ReactJS, Tailwind CSS, and other technologies. I also develop applications, automation 
+    flows, lists, and libraries through the Power Platform, utilizing technologies such as Power
+     Apps, Power Automate, and Office 365 tools, as well as managing SharePoint flows and pages.
+      I have a strong passion for technology and development, especially in the area of web development. 
+      I possess skills in JavaScript, HTML, CSS, ReactJS, Svelte, Figma and Python. Additionally, 
+      I am actively learning and expanding my knowledge in popular frameworks and libraries like 
+      Angular and ReactJS. I am known for being quick, a fast and consistent learner, always motivated 
+      to overcome obstacles and embrace new challenges.
     </Text>
   </Section>
 );
