ea4a50af4b18d86c94743a39d09a029bc3c467f9
vlecabr <vitartasleca@gmail.com>
2025-02-03 11:04:57 -0300

    atualizado hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -15,7 +15,7 @@ const Hero = () => (
     <Subtitle>I build things for the web.</Subtitle>
     <Description>
       A young computer science student focused on web development, with professional experience and 4 years in multinationals companies. <br></br>
-      Quick to learn and adept at working with the latest web technologies, such as React.JS, HTML5, CSS3, SASS, Figma, Svelte, Python, and more. <br></br>
+      Quick to learn and adept at working with the latest web technologies, such as React.JS, JavaScript, HTML5, CSS3, NodeJS, SASS, Figma, Svelte, Python, and more. <br></br>
       Always eager to learn more.
     </Description>
     <Button href="https://www.linkedin.com/in/victor-leca-vlkbr/" target="_blank">Check out my Linkedin</Button>
