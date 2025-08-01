971bac95db5941473ea119ef5b5930b7af62f67c
vlecabr <vitartasleca@gmail.com>
2025-08-01 12:11:46 -0300

    ajustando codigo para fullstack

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -12,13 +12,13 @@ const About = () => (
     <Text>
 I'm a Computer Science student with 4 years of experience in multinational companies. 
 I began my professional journey in 2021 focusing on technical support and infrastructure, 
-but I currently work in development, with a strong emphasis on automation and web development.
- I’m responsible for building websites using technologies like ReactJS and Tailwind CSS. 
- I also develop applications, automation flows, lists, and libraries using the Power Platform 
- such as Power Apps, Power Automate, and Office 365 tools and manage SharePoint pages and workflows.
-  I'm passionate about technology and software development, especially in the web environment. 
-  I'm constantly learning and improving my development skills. I'm known for being fast, a quick 
-  and consistent learner, always motivated to overcome obstacles and embrace new challenges.
+but I currently work in development, with a strong emphasis on automation and web development. 
+I’m responsible for building websites using technologies like ReactJS and Tailwind CSS and in the backend 
+i use NodeJS. I also develop applications, automation flows, lists, and libraries using the Power Platform 
+such as Power Apps, Power Automate, and Office 365 tools and manage SharePoint pages and workflows. 
+I'm passionate about technology and software development, especially in the web environment. 
+I'm constantly learning and improving my development skills. I'm known for being fast, a quick and 
+consistent learner, always motivated to overcome obstacles and embrace new challenges.
     </Text>
   </Section>
 );
