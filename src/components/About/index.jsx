6c34793484159eec6f87744ff0e14c29b8b64f40
vlecabr <vitartasleca@gmail.com>
2025-08-06 16:41:49 -0300

    about melhorado

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Section, Title, Text } from './About.styles';
 import { useLanguage } from '../../context/LanguageContext';
 
-const About = () => {
-  const { lang } = useLanguage();
-
-  const text = {
-    en: {
-      title: "About Me",
-      content: `I'm a Computer Science student with 4 years of experience in multinational companies. 
+const TEXTS = {
+  en: {
+    title: 'About Me',
+    content: `I'm a Computer Science student with 4 years of experience in multinational companies. 
       I began my professional journey in 2021 focusing on technical support and infrastructure, 
       but I currently work in development, with a strong emphasis on automation and web development. 
       I’m responsible for building websites using technologies like ReactJS and Tailwind CSS and in the backend 
@@ -17,10 +14,10 @@ const About = () => {
       I'm passionate about technology and software development, especially in the web environment. 
       I'm constantly learning and improving my development skills. I'm known for being fast, a quick and 
       consistent learner, always motivated to overcome obstacles and embrace new challenges.`,
-    },
-    pt: {
-      title: "Sobre Mim",
-      content: `Sou estudante de Ciência da Computação com 4 anos de experiência em empresas multinacionais. 
+  },
+  pt: {
+    title: 'Sobre Mim',
+    content: `Sou estudante de Ciência da Computação com 4 anos de experiência em empresas multinacionais. 
       Iniciei minha jornada profissional em 2021 focando em suporte técnico e infraestrutura, 
       mas atualmente trabalho com desenvolvimento, com forte ênfase em automação e desenvolvimento web. 
       Sou responsável por construir sites usando tecnologias como ReactJS e Tailwind CSS e no backend uso NodeJS. 
@@ -29,15 +26,19 @@ const About = () => {
       Sou apaixonado por tecnologia e desenvolvimento de software, especialmente no ambiente web. 
       Estou sempre aprendendo e melhorando minhas habilidades de desenvolvimento. Sou conhecido por ser rápido, 
       um aprendiz consistente e ágil, sempre motivado para superar obstáculos e abraçar novos desafios.`,
-    }
-  };
+  },
+};
+
+const About = () => {
+  const { lang } = useLanguage();
+  const text = TEXTS[lang];
 
   return (
     <Section id="about">
-      <Title>{text[lang].title}</Title>
-      <Text>{text[lang].content}</Text>
+      <Title>{text.title}</Title>
+      <Text>{text.content}</Text>
     </Section>
   );
 };
 
-export default About;
+export default memo(About);
